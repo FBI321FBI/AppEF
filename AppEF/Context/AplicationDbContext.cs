0388cfdb80c78d9912c3d4290f099fe917a55e9a
Ilya Post <im-popow13@mail.ru>
2024-05-14 22:25:50 +0500

    1

---
+++ AppEF/Context/AplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EFProject.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFProject.Context
+{
+    class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationDbContext() => Database.EnsureCreated(); // Проверка и создание бд, если она отсутствует
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.LogTo(Console.WriteLine);
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=postgres");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Объект -> свойство -> метод который настраивает св-во
+            modelBuilder.Entity<User>().Property(x => x.Id)
+                .ValueGeneratedOnAdd();
+            modelBuilder.Entity<User>().Property(x => x.Name)
+                .HasMaxLength(100)
+                .HasColumnName("FirstName");
+        }
+    }
+}
---
+++ AppEF/View/WindowAccess.xaml.cs
@@ -0,0 +1,46 @@
+﻿using AppEF.View;
+using EFProject.Context;
+using System.Linq;
+using System.Windows;
+
+namespace AppEF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class WindowAccess : Window
+    {
+        public WindowAccess()
+        {
+            InitializeComponent();
+        }
+
+        private void bAccess_Click(object sender, RoutedEventArgs e)
+        {
+            if (AccessUser())
+            {
+                MessageBox.Show("Access granted");
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            } 
+            else
+                MessageBox.Show("Not found");
+        }
+
+        public bool AccessUser()
+        {
+            using (ApplicationDbContext db = new ApplicationDbContext())
+            {
+                var users = db.Users.ToList();
+                foreach (var user in users)
+                    if (user.Name == textBoxLog.Text)
+                        if (user.Password == textBoxPass.Text)
+                            return true;
+                        else
+                            return false;
+            }
+            return false;
+        }
+    }
+}
